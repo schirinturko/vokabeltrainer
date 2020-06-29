5119a9182b0bffdc044e73103a72b88686c8448c
schirinturko <schirin.turko24@gmail.com>
2020-06-30 00:22:47 +0200

    add crud methods

--- vokabeln/src/main/java/at/htl/boundary/TrafficEndpoint.java
+++ vokabeln/src/main/java/at/htl/boundary/TrafficEndpoint.java
@@ -1,10 +1,14 @@
 package at.htl.boundary;
 
 import at.htl.control.TrafficRepository;
+import at.htl.entity.Traffic;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-import javax.ws.rs.Path;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.util.List;
 
 @Path("/traffic")
 @Transactional
@@ -12,4 +16,75 @@ public class TrafficEndpoint {
 
     @Inject
     TrafficRepository trafficRepository;
+
+    //hello - test
+    @GET
+    @Path("/hello")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String hello() {
+        return "hello (Traffic)";
+    }
+
+    //findAllFood
+    @GET
+    @Path("/findAllTraffic")
+    /*public List<Traffic> findAllTraffic(){
+        return trafficRepository.listAll();
+    } ODER: */
+    public Response findAllTraffic(){
+        List<Traffic> findAllTraffic = trafficRepository.listAll();
+        return Response.ok(findAllTraffic).build();
+    }
+
+    //find traffic word by id
+    @GET
+    @Path("/{id}")
+    public Response findTrafficById(@PathParam("id") Long id){
+        Traffic traffic = trafficRepository.findById(id);
+        return Response.ok(traffic.getENword()).build();
+    }
+
+    //create traffic word -> Repo
+    @POST
+    @Path("/createTraffic")
+    public Response createTraffic(Traffic traffic){
+        trafficRepository.createTraffic(traffic);
+        return Response.ok().build();
+    }
+
+    //update traffic word by id -> Repo
+    @PUT
+    @Path("/updateTraffic/{id}")
+    public Response updateTraffic(Traffic traffic, @PathParam("id") Long id){
+        return Response.ok().build();
+    }
+
+    //delete traffic word by id
+    @DELETE
+    @Path("/deleteTraffic/{id}")
+    public Response deleteTrafficById(@PathParam("id") Long id){
+        trafficRepository.delete("id", id);
+        return Response.ok().build();
+    }
+
+    //delete all traffic words
+    @DELETE
+    @Path("/deleteTraffic")
+    public Response deleteTraffic() {
+        trafficRepository.deleteAll();
+        return Response.ok().build();
+    }
+
+    //send answer
+    @POST
+    @Path("/{id}")
+    public Response sendTrafficAnswer(@PathParam("id") Long id, @QueryParam("answer") String answer) {
+        Traffic traffic = trafficRepository.findTrafficById(id);
+        if (answer.equals(traffic.getGERword())) {
+            return Response.ok(false).build();
+        } else {
+            return Response.ok(true).build();
+        }
+        //return Response.ok(answer).build();
+    }
 }
